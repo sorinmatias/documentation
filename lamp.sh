ba7205eab871f2472dae04fd9ceeb08aa781c1a8
root <root@sorinmatias1.mylabserver.com>
2018-02-19 12:07:18 +0000

    final lamp

--- lamp.sh
+++ lamp.sh
@@ -1,8 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
 #Created by Sorin Matias sorin.matias@evozon.com
 
 #Installing selected webserver
+echo -e "Installing cur yum-utils wget. Please wait..."
+yum -y -q install curl yum-utils wget 2>&1>/dev/null
+echo -e "Cleaning yum cache"
+yum -q clean all 2>&1
 WEBSERVER=0
 echo -e "\nPlease select your desired webserver\nPlease enter 1 for Apache or 2 for Nginx:"
 read WEBSERVER
@@ -14,53 +18,54 @@ done
 if  [ $WEBSERVER -eq 1 ]
 then
     echo "Installing Apache WebServer"
+    yum -y -q install mod_ssl openssl 2>&1>/dev/null
     yum -y install httpd
+    curl https://linuxtm.ro/webservers/ |sed -n '68,137p' | sed 's/<pre>//g' |sed 's/\&lt;/</g' | sed 's/\&gt;/>/g' | sed 's/RewriteEngine/#RewriteEngine/g' | sed 's/RewriteCond/#RewriteCond/g' | sed 's/RewriteRule/#RewriteRule/g' | sed 's/^/#/' > /etc/httpd/conf.d/default.conf
+    curl https://linuxtm.ro/webservers/ |sed -n '141,169p' | sed 's/<pre>//g' |sed 's/\&lt;/</g' | sed 's/\&gt;/>/g' | sed 's/^/#/' > /etc/httpd/conf.d/default-ssl.conf
     systemctl enable httpd
     systemctl start httpd
 else
     echo "Installing nginx WebServer and PHP-FPM"
     yum -y install nginx
+    curl https://linuxtm.ro/webservers/ | sed -n '177,265p' | sed 's/<pre>//g' |sed 's/\&lt;/</g' | sed 's/\&gt;/>/g' | sed 's/^/#/' > /etc/nginx/conf.d/default.conf
     systemctl enable nginx
     systemctl start nginx
 fi
 
 #Installing selected Mariadb
-MARIADEFAULT=5.5
-
-echo -e "\nWarning MariaDB 10.3 is Beta Release!\nPlease enter the desired MariaDB version (10.0 or 10.1 or 10.2 or 10.3 or enter for default)"
+echo -e "\nWarning MariaDB 10.3 is Beta Release!\nPlease enter the desired MariaDB version (10.0 or 10.1 or 10.2 or 10.3 or enter for default 5.5)"
 read ANSWER;
 while [ "$ANSWER" != "" ] && [ "$ANSWER" != "10.0" ] && [ "$ANSWER" != "10.1" ] && [ "$ANSWER" != "10.2" ] && [ "$ANSWER" != "10.3" ]; do
     echo "Please enter a valid answer (10.0 or 10.1 or 10.2 or 10.3 or enter for default)"
     read ANSWER;
     echo "$ANSWER";
 done
-if [ "$ANSWER" == "" ]
-then
-    echo "Installing default mariadb"
-    echo -e "# MariaDB $MARIADEFAULT CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$MARIADEFAULT/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
-    yum -y install MariaDB-server MariaDB-client
-elif [ "$ANSWER" == "10.0" ];then
-    echo "Installing $ANSWER mariadb"
-    echo -e "# MariaDB $ANSWER CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$ANSWER/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
-    yum -y install MariaDB-server MariaDB-client
-elif [ "$ANSWER" == "10.1" ];then
-    echo "Installing $ANSWER mariadb"
-    echo -e "# MariaDB $ANSWER CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$ANSWER/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
-    yum -y install MariaDB-server MariaDB-client
-elif [ "$ANSWER" == "10.2" ];then
-    echo "Installing $ANSWER mariadb"
-    echo -e "# MariaDB $ANSWER CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$ANSWER/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
-    yum -y install MariaDB-server MariaDB-client
-elif [ "$ANSWER" == "10.3" ];then
-    echo "Installing $ANSWER mariadb"
-    echo -e "# MariaDB $ANSWER CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$ANSWER/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
-    yum -y install MariaDB-server MariaDB-client
-    systemctl enable mariadb
+if [ "$ANSWER" == "" ];then
+    ANSWER=5.5
 fi
-systemctl start mariadb
+#    echo "Installing default mariadb"
+ #   echo -e "# MariaDB $MARIADEFAULT CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$MARIADEFAULT/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
+#    yum -y install MariaDB-server MariaDB-client
+#elif [ "$ANSWER" == "10.0" ];then
+    echo "Installing $ANSWER MariaDB"
+    echo -e "# MariaDB $ANSWER CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$ANSWER/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
+#    yum -y install MariaDB-server MariaDB-client
+#elif [ "$ANSWER" == "10.1" ];then
+#    echo "Installing $ANSWER mariadb"
+#    echo -e "# MariaDB $ANSWER CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$ANSWER/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
+#    yum -y install MariaDB-server MariaDB-client
+#elif [ "$ANSWER" == "10.2" ];then
+#    echo "Installing $ANSWER mariadb"
+#    echo -e "# MariaDB $ANSWER CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$ANSWER/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
+#    yum -y install MariaDB-server MariaDB-client
+#elif [ "$ANSWER" == "10.3" ];then
+#    echo "Installing $ANSWER mariadb"
+#    echo -e "# MariaDB $ANSWER CentOS repository list\n# http://downloads.mariadb.org/mariadb/repositories/\n[mariadb]\nname = MariaDB\nbaseurl = http://yum.mariadb.org/$ANSWER/centos7-amd64\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB\ngpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
+yum -y install MariaDB-client
+systemctl enable MariaDB
+systemctl start MariaDB
 
 #Installing selected php
-yum -y install yum-utils wget
 echo -e "\nPlease enter the desired PHP version (5.4 or 5.5 or 5.6 or 7.0 or 7.1 or 7.2 enter for default)"
 read ANSWER;
 while [ "$ANSWER" != "" ] && [ "$ANSWER" != "5.4" ] && [ "$ANSWER" != "5.5" ] && [ "$ANSWER" != "5.6" ] && [ "$ANSWER" != "7.0" ] && [ "$ANSWER" != "7.1" ] && [ "$ANSWER" != "7.2" ]; do
@@ -68,15 +73,16 @@ while [ "$ANSWER" != "" ] && [ "$ANSWER" != "5.4" ] && [ "$ANSWER" != "5.5" ] &&
     read ANSWER;
     echo "$ANSWER";
 done
-if ["$ANSWER" == "" ]; then
-  $ANSWER=5.4
+if [ "$ANSWER" == "" ]; then
+  ANSWER=5.4
 fi
 echo "Download Epel Repository"
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -P /etc/yum.repos.d/
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -O /etc/yum.repos.d/epel-release-latest-7.noarch.rpm
 rpm -Uvh /etc/yum.repos.d/epel-release-latest-7.noarch.rpm
 echo -e "\n\n remi \n\n"
-wget http://rpms.famillecollet.com/enterprise/remi.repo -P /etc/yum.repos.d/
+wget http://rpms.famillecollet.com/enterprise/remi.repo -O /etc/yum.repos.d/remi.repo
 
+yum-config-manager --disable remi-php55 remi-php56 remi-php70 remi-php71 remi-php72
 yum-config-manager --enable remi-php${ANSWER//.}
 yum install -y php php-mysql
 echo "Would you like to install some common PHP modules that are required by CMS Systems like Wordpress, Joomla and Drupal (y / n)"
@@ -92,3 +98,4 @@ if [ $WEBSERVER -eq 2 ];then
     yum -y install php-fpm
     systemctl start php-fpm
 fi
+
